fefa292458eebaf2f157b1602eec44581810d384
Polly Smith <pollylestersmith@outlook.com>
2020-08-16 19:58:15 +0100

    New commit with html served

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ const path = require('path');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
+    if(req.url === '/') {
+        fs.readFile(path.join(__dirname, 'index-mongo.html'), (err, content => {
+            if (err) throw err;
+            res.end(content);
+        })
+    }
 });
 
 
-/*     if(req.url === '/') {
-        fs.readFile(path.join(__dirame, 'index-mongo.html'), (err, content) => {
-            if (err) throw err;
-            res.end(content);
-        }
-    )}}) */
 
 const PORT = process.env.port || 3000;
 server.listen(PORT, () => console.log(`server running on port ${PORT}`));
