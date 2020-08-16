254b9248efca9d233a5fe324a29da67d30063a83
Polly Smith <pollylestersmith@outlook.com>
2020-08-16 15:16:32 +0100

    Now with a server fixed

--- todo-mongo.js
+++ todo-mongo.js
@@ -4,13 +4,12 @@ const path = require('path');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    if(req.url === '/' {
+    if(req.url === '/') {
         fs.readFile(path.join(__dirame, 'index-mongo.html'), (err, content) => {
             if (err) throw err;
             res.end(content);
-        })
-    })
-})
+        }
+    )}})
 
 const PORT = process.env.port || 3000;
 app.listen(PORT, () => console.log(`server running on port ${port}`));
