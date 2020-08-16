5f32c3b51d2286c18a9a49da4753e1d8901db59e
Polly Smith <pollylestersmith@outlook.com>
2020-08-16 20:22:01 +0100

    Trying a console log again

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     if(req.url === '/') {
+        console.log('request received');
         fs.readFile(path.join(__dirname, 'index-mongo.html'), (err, content => {
             if (err) throw err;
-            res.end('hi?');
+            res.end(content);
         }))
     }
 });
