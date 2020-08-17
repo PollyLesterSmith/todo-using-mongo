3f48e0b9297349e3fc05569719ec13a6f957d5f5
Polly Smith <pollylestersmith@outlook.com>
2020-08-17 11:41:35 +0100

    Heroku test 1

--- app.js
+++ app.js
@@ -20,5 +20,5 @@ const server = http.createServer((req, res) => {
 
 
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`server running on port ${PORT}`));
