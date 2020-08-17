bda5081a00b5b5cd07ef89ac1804d56361ace412
Polly Smith <pollylestersmith@outlook.com>
2020-08-17 12:35:14 +0100

    Need to make a server that has filetypes 2

--- app.js
+++ app.js
@@ -4,18 +4,30 @@ const path = require('path');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-     if(req.url === '/') {
-        const serveFilename = path.join(__dirname, 'index-mongo.html');
-        fs.readFile(serveFilename, (err, content) => {
-            if (err) throw err;
-            renderContent(content);
-        })
-        
-        function renderContent(content) {
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(content);
-        }
+
+    // Init file path and content type
+    let filePath = (req.url === '/' ? path.join(__dirname, 'index-mongo.html') : path.join(__dirname, req.url));
+    let extName = path.extname(filePath);
+    let contentType = "text/html";
+    switch (extName) {
+        case 'html' : contentType = "text/html"
+        break;
+        case 'css' : contentType = "text/css"
+        break;
     }
+        
+    fs.readFile(filePath, (err, content) => {
+        if (err) throw err;
+        res.writeHead(200, { 'Content-Type': contentType });
+        renderContent(content);
+    })
+
+    function renderContent(content) {
+        res.write(content)
+    }
+
+    res.end();
+
 });
 
 
