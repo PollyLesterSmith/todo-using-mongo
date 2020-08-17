7254bda19df96b6429e14af67eaa4c2f10d63d87
Polly Smith <pollylestersmith@outlook.com>
2020-08-17 11:57:23 +0100

    More testing

--- app.js
+++ app.js
@@ -5,17 +5,14 @@ const fs = require('fs');
 
 const server = http.createServer((req, res) => {
      if(req.url === '/') {
-        console.log('request received');
         const serveFilename = path.join(__dirname, 'index-mongo.html');
-        console.log(serveFilename);
-        fs.readFile(serveFilename, (err, content) => {
+        
+        fs.readFileSync(serveFilename, (err, content) => {
             if (err) throw err;
             res.writeHead(200, { 'Content-Type': 'text/html' });
             res.end(content);
         })
     }
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end(content);
 });
 
 
